9ed71558c1ed61512ffa53f1aa88febdc63599a6
Bruno Jacob <87205957+brunojacob89@users.noreply.github.com>
2022-04-13 13:24:48 -0300

    Add files via upload

---
+++ TDD e Java2/2108-java-testes-unitarios-tdd-projeto_inicial/src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,49 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+class BonusServiceTest {
+
+	@Test
+	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
+		
+		BonusService service = new BonusService();
+//		assertThrows(IllegalArgumentException.class,
+//										() -> service.calcularBonus(new Funcionario("Bruno", LocalDate.now(), new BigDecimal("25000"))));
+
+		try {
+			service.calcularBonus(new Funcionario("Bruno", LocalDate.now(), new BigDecimal("25000")));
+			fail("não deu a exception!");
+		} catch (Exception e) {
+
+			assertEquals("Funcionário com salário maior do que R$10000 nao pode receber bonus!", e.getMessage());
+		}
+		
+	}
+	
+	@Test
+	void bonusDeveriaSerDezPorCentoDoSalario() {
+		
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Bruno", LocalDate.now(), new BigDecimal("2500")));
+		
+		assertEquals(new BigDecimal("250.00"), bonus);
+	}
+	
+	@Test
+	void bonus() {
+		
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(new Funcionario("Bruno", LocalDate.now(), new BigDecimal("10000")));
+		
+		assertEquals(new BigDecimal("1000.00"), bonus);
+	}
+
+}
